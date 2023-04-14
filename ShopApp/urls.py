4a9e5b88591cf6792ae691a5a21d79c9180a96c7
oganibi <oganibi@gmail.com>
2023-04-14 02:10:40 +0100

    product page and Pagination

--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -3,4 +3,5 @@ from ShopApp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('product/', views.product, name='product'),
 ]
--- ShopApp/views.py
+++ ShopApp/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Product, Customer, Order
 
 # Create your views here.
 
 
 def index(request):
+
     return render(request, 'ShopApp/index.html')
+
+
+def product(request):
+    products = Product.objects.all()
+    paginator = Paginator(products, 10)
+    page_number = request.GET.get('page', 1)
+    page_list = paginator.page(page_number)
+    try:
+        page_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_list = paginator.page(1)
+    except EmptyPage:
+        page_list = paginator.page(paginator.num_pages)
+    return render(request, 'ShopApp/product.html', {'page_list': page_list, 'range': range(1, 11)})
